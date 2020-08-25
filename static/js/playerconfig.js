07942d795c907cbf24323133e19f04e7fdb3dbdd
fabu <55358617+fb999999@users.noreply.github.com>
2020-08-25 22:11:19 +0800

    20200825

---
+++ static/js/playerconfig.js
@@ -0,0 +1,7 @@
+﻿var MacPlayerConfig={};
+//参数开始
+MacPlayerConfig={"width":"100%","height":"100%","widthmob":"100%","heightmob":"100%","widthpop":"0","heightpop":"600","second":"0","prestrain":"","buffer":"","parse":"https:\/\/img.zhin8.com\/jiexi\/?url=","autofull":"0","showtop":"1","showlist":"1","flag":"0","colors":"000000,F6F6F6,F6F6F6,333333,666666,FFFFF,FF0000,2c2c2c,ffffff,a3a3a3,2c2c2c,adadad,adadad,48486c,fcfcfc"};
+//参数结束
+//缓存开始
+MacPlayerConfig.player_list={"ybd":{"show":"\u4e91\u64adD","des":"pptv.com","ps":"0","parse":""},"ybw":{"show":"\u4e91\u64adW","des":"pptv.com","ps":"0","parse":""},"ybo":{"show":"\u4e91\u64adO","des":"pptv.com","ps":"0","parse":""},"yb605":{"show":"\u4e91\u64ad6","des":"pptv.com","ps":"0","parse":""},"ybc":{"show":"\u4e91\u64adC","des":"","ps":"0","parse":""},"ybk":{"show":"\u4e91\u64adK","des":"pptv.com","ps":"0","parse":""},"yby":{"show":"\u4e91\u64adY","des":"pptv.com","ps":"0","parse":""},"qiyi":{"show":"\u7231\u5947\u827a","des":"","ps":"0","parse":""},"xigua":{"show":"\u897f\u74dc\u4e91","des":"","ps":"0","parse":""},"ybm":{"show":"\u4e91\u64adM","des":"","ps":"0","parse":""}},MacPlayerConfig.downer_list={"xunlei":{"show":"xunlei\u4e0b\u8f7d","des":"des\u63d0\u793a\u4fe1\u606f","ps":"0","parse":""},"http":{"show":"http\u4e0b\u8f7d","des":"des\u63d0\u793a\u4fe1\u606f","ps":"0","parse":""}},MacPlayerConfig.server_list={"server1":{"show":"\u6d4b\u8bd5\u670d\u52a1\u56681","des":"des\u63d0\u793a\u4fe1\u606f1"}};
+//缓存结束
